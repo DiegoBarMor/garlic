abf5cb423270557717a93e49f1aa685b8cc1b5a7
DBM <diegobarmor42@gmail.com>
2026-03-22 17:02:58 +0100

    added generic SoundEffect management

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GARLIC
 **GARLIC** (GUI Applications with RayLib In C) is a simple framework fo making GUI applications in C.
+
+# Considerations
+When using garlic's functions, the following data must be initialized statically somewhere in the source files:
+```c
+GarlicTexture TEXTURES[]; // used by load_textures(), unload_textures(), getp_texture()
+SoundEffect SOUND_EFFECTS[]; // used by load_sound_effects(), unload_sound_effects(), play_sfx(), play_sfx_random()
+bool SOUND_SFX_ON; // used by play_sfx(), play_sfx_random()
+```
--- garlic.c
+++ garlic.c
@@ -1,9 +1,11 @@
-////// GARLIC V0.1.0 (2026-03-21)
-//// When using garlic's functions, the following data must be
-//// initialized statically somewhere in the source files
-////    GarlicTexture TEXTURES[]; // used by load_textures(), unload_textures(), getp_texture()
+////// GARLIC V0.1.0 (2026-03-22)
+////// IMPORTANT: read Considerations in README.md
 
 #include "garlic.h"
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// GRAPHICS ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 // -----------------------------------------------------------------------------
 void load_textures(unsigned int n) {
@@ -29,6 +31,36 @@ static Texture2D _load_texture_resize(GarlicTexture* gt) {
     Texture2D tex = LoadTextureFromImage(img);
     UnloadImage(img);
     return tex;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// AUDIO /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// -----------------------------------------------------------------------------
+void load_sound_effects(unsigned int n) {
+    for (SoundEffect* sfx = SOUND_EFFECTS; sfx < SOUND_EFFECTS + n; sfx++)
+        sfx->sound = LoadSound(sfx->path_sfx);
+}
+
+// -----------------------------------------------------------------------------
+void unload_sound_effects(unsigned int n) {
+    for (SoundEffect* sfx = SOUND_EFFECTS; sfx < SOUND_EFFECTS + n; sfx++)
+        UnloadSound(sfx->sound);
+}
+
+// -----------------------------------------------------------------------------
+void play_sfx(unsigned int idx) {
+    if (!SOUND_SFX_ON) return;
+    PlaySound(SOUND_EFFECTS[idx].sound);
+}
+
+// -----------------------------------------------------------------------------
+void play_sfx_random(unsigned int idx, unsigned int n_variants) {
+    if (!SOUND_SFX_ON) return;
+    int choice = GetRandomValue(idx, idx + n_variants - 1);
+    PlaySound(SOUND_EFFECTS[choice].sound);
 }
 
 // -----------------------------------------------------------------------------
